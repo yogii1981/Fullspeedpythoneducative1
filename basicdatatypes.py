1ab068abeb9080e14c15c273a66fa9cdf632bd56
yogeshsharma <yogeshsharma1981@gmail.com>
2020-03-12 23:35:46 -0400

    commit
    Signed-off-by: yogeshsharma <yogeshsharma1981@gmail.com>

---
+++ basicdatatypes.py
@@ -0,0 +1,14 @@
+n1 = int(input("Enter a number:"))  # input enter a number 1
+n2 = int(input("Enter a number:"))  # input enter a number 2
+
+
+def mathop(n1, n2):  # define a function "mathop" and has two variables n1, n2
+    classic_division = n1/n2
+    floor_division = n1//n2
+    modulus = n1 % n2
+    power = n1 ** n2
+    return [classic_division, floor_division, modulus, power]
+
+
+test = mathop(n1, n2)  # calling the mathop function
+print(test)
