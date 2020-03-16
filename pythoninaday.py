ab733fc4307c79959faf4d226bd1af90826b61ed
yogeshsharma <yogeshsharma1981@gmail.com>
2020-03-15 20:21:14 -0400

    commit
    Signed-off-by: yogeshsharma <yogeshsharma1981@gmail.com>

--- pythoninaday.py
+++ pythoninaday.py
@@ -253,3 +253,30 @@ result = print("{} - {} = {}".format(a, b, sub))
 # Determine the output withour running code
 
 print(''' Date: \nJan11, 2019 Time: \n1.28pm Venue:\nConvention Center Number of Pax:\n30''')
+
+# chapter5 question 5
+
+print('This is a single quotation (\')mark and this is a double (") mark.')
+
+# chapter6 question 6
+The
+code
+below
+shows
+the
+last
+few
+lines
+of
+a
+program:
+day = {1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday', 7: 'Monday'}
+venue = {1: 'Tokyo to Osaka', 2: 'Osaka', 3: 'Kyoto', 4: 'Kyoto to Nara', 5: 'Nara to Osaka', 6: 'Osaka to Tokyo',
+         7: 'Tokyo'}
+print('Day 1 (%s): %s' % (day[1], venue[1]))
+print('Day 2 (%s): %s' % (day[2], venue[2]))
+print('Day 3 (%s): %s' % (day[3], venue[3]))
+print('Day 4 (%s): %s' % (day[4], venue[4]))
+print('Day 5 (%s): %s' % (day[5], venue[5]))
+print('Day 6 (%s): %s' % (day[6], venue[6]))
+print('Day 7 (%s): %s' % (day[7], venue[7]))
