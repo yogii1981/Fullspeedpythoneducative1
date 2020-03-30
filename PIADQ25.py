5b910b66013a88a4fd4af7af0da314d9b9901ba4
yogeshsharma <yogeshsharma1981@gmail.com>
2020-03-30 15:15:40 -0400

    commit
    Signed-off-by: yogeshsharma <yogeshsharma1981@gmail.com>

---
+++ PIADQ25.py
@@ -0,0 +1,8 @@
+# pythonin a day chapter 6 question 25
+# modify the code using "continue" keyword so that even numbers are not printed:
+
+
+for i in range(10):
+    if i % 2 == 0:
+        continue
+    print('i= ', i)
