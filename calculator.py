59e9e36bea96688be0809e7286fcd28c0da8aa26
yogeshsharma <yogeshsharma1981@gmail.com>
2020-03-31 11:50:07 -0400

    commit
    Signed-off-by: yogeshsharma <yogeshsharma1981@gmail.com>

---
+++ calculator.py
@@ -0,0 +1,63 @@
+# write a calculator program where user enters two numbers
+# and also use try and except to perform error handling
+# in this program has to enter the integer again and again and
+# not efficent method
+
+
+class Calculator:
+
+    def ___init__(self, action=None):
+        self.action = action
+
+    def calculateQuotient(self):
+        try:
+            integer1 = int(input("Enter an integer1:"))
+            integer2 = int(input("Enter an integer2:"))
+            result = int(integer1 / integer2)
+            return result
+        except:
+            return -1
+
+    def addition(self):
+        try:
+            integer1 = int(input("Enter an integer1:"))
+            integer2 = int(input("Enter an integer2:"))
+            resultaddition = int(integer1 + integer2)
+            return resultaddition
+        except:
+            return -1
+
+    def subtraction(self):
+        try:
+            integer1 = int(input("Enter an integer1:"))
+            integer2 = int(input("Enter an integer2:"))
+            resultsubtraction = int(integer1 - integer2)
+            return resultsubtraction
+        except:
+            return -1
+
+    def multiplication(self):
+        try:
+            integer1 = int(input("Enter an integer1:"))
+            integer2 = int(input("Enter an integer2:"))
+            resultmuliplication = int(integer1 * integer2)
+            return resultmuliplication
+        except:
+            return -1
+
+    def remainder(self):
+        try:
+            integer1 = int(input("Enter an integer1:"))
+            integer2 = int(input("Enter an integer2:"))
+            resultremainder = int(integer1 % integer2)
+            return resultremainder
+        except:
+            return -1
+
+
+calculate = Calculator()
+print(calculate.calculateQuotient())
+print(calculate.multiplication())
+print(calculate.addition())
+print(calculate.subtraction())
+print(calculate.remainder())
