1c2060353357503a711fcb67aca93108f04b8c5c
yogeshsharma <yogeshsharma1981@gmail.com>
2020-04-02 22:20:17 -0400

    commit
    Signed-off-by: yogeshsharma <yogeshsharma1981@gmail.com>

---
+++ EIPC6E2.py
@@ -0,0 +1,7 @@
+# write a program to find minimum between two numbers
+a = int(input("Enter First Number:"))
+b = int(input("Enter Second Number:"))
+if a > b:
+    print(" b is minimum which is", b)
+else:
+    print("a is minimum which is", a)
