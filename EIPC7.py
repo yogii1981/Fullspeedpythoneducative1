c32755a6b8eb623a36680a3352ef79486d3a31ad
yogeshsharma <yogeshsharma1981@gmail.com>
2020-04-03 20:48:14 -0400

    commit
    Signed-off-by: yogeshsharma <yogeshsharma1981@gmail.com>

---
+++ EIPC7.py
@@ -0,0 +1,14 @@
+import math
+
+print(int(math.sqrt(16)))
+
+# calaculate the area of circle
+r = 27.6
+
+import math as m
+
+print(m.pi * r ** 2)
+print("The ceil of r is:", r, end=" ")
+print(math.ceil(r))
+print("the floor of r is:", r, end=" ")
+print(math.floor(r))
