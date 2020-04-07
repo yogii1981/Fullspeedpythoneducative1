0db28412702b889b3473b47fe97d7b4a87e6e370
yogeshsharma <yogeshsharma1981@gmail.com>
2020-04-07 08:19:35 -0400

    commit
    Signed-off-by: yogeshsharma <yogeshsharma1981@gmail.com>

--- inhertiancecalculator.py
+++ inhertiancecalculator.py
@@ -17,11 +17,20 @@ class Calculator:
         resultsubtraction = int(self.integer1 - self.integer2)
         return resultsubtraction
 
+    def multiplication(self):
+        resultmultitiplication = int(self.integer1 * self.integer2)
+        return resultmultitiplication
+
+    def division(self):
+        resultdivision = int(self.integer1 / self.integer1)
+        return resultdivision
+
 
 class Calculateadvance(Calculator):
-    def __init__(self, integer1=None, integer2=None, squareroot=None):
+    def __init__(self, integer1=None, integer2=None, squareroot=None, cuberoot=None):
         Calculator.__init__(self, integer1, integer2)
         self.squareroot = squareroot
+        self.cuberoot = squareroot
 
     def squarerootcalculation(self):
         n = 2
@@ -32,6 +41,32 @@ class Calculateadvance(Calculator):
             self.integer2 = self.integer2 ** 0.5
             print(self.integer1, self.integer2)
 
+    def cuberootcalculation(self):
+        n = 3
+        self.enternumber()
+        print(self.integer1, self.integer2)
+        if self.integer1 != 0 and self.integer2 != 0:
+            self.integer1 = self.integer1 ** (1. / 3)
+            self.integer2 = self.integer2 ** (1. / 3)
+            print(self.integer1, self.integer2)
+
+    def squarecalculation(self):
+        self.enternumber()
+        print(self.integer1, self.integer2)
+        self.integer1 = self.integer1 ** 2
+        self.integer2 = self.integer2 ** 2
+        print(self.integer1, self.integer2)
+
+    def cubecalculation(self):
+        self.enternumber()
+        print(self.integer1, self.integer2)
+        self.integer1 = self.integer1 ** 3
+        self.integer2 = self.integer2 ** 3
+        print(self.integer1, self.integer2)
+
 
 specialcalculation = Calculateadvance()
 specialcalculation.squarerootcalculation()
+specialcalculation.cuberootcalculation()
+specialcalculation.squarecalculation()
+specialcalculation.cubecalculation()
