2f42ce01bbda4d82658186f03c6b26149850299a
yogeshsharma <yogeshsharma1981@gmail.com>
2020-04-07 18:36:59 -0400

    commit
    Signed-off-by: yogeshsharma <yogeshsharma1981@gmail.com>

---
+++ experitnpython/EIPC10forloop.py
@@ -0,0 +1,9 @@
+# program to print characters present in the given strings
+s = "python book"
+for i in s:
+    print(i)
+
+# program to print characters present in the given strings and adding a comma to separate character
+s = "python book"
+for i in s:
+    print(i, end=',')
