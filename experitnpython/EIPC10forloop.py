f970203b05d50e85bae833ce7b5d3b91cf5a6e72
yogeshsharma <yogeshsharma1981@gmail.com>
2020-04-07 21:06:42 -0400

    commit
    Signed-off-by: yogeshsharma <yogeshsharma1981@gmail.com>

--- experitnpython/EIPC10forloop.py
+++ experitnpython/EIPC10forloop.py
@@ -7,3 +7,35 @@ for i in s:
 s = "python book"
 for i in s:
     print(i, end=',')
+
+# To print characters present in string index wise:
+s = input("Enter your string:")
+i = 0
+for x in s:
+    print("The character present at", i, "index is:", x)
+    i = i + 1
+
+# To print Hello 10 times
+for x in range(10):
+    print("test")
+
+# To display numbers from 0 to 10
+for x in range(0, 11):
+    print(x)
+
+# To display the odd numbers between 0 to 21
+for x in range(21):
+    if (x % 2 != 0):
+        print(x)
+
+# to display the number from 10 to 0 in descending order
+for x in range(10, 0, -1):
+    print(x)
+
+# to display the sum of integers in list
+
+list = eval(input("Enter list:"))
+sum = 0
+for x in list:
+    sum = sum + x
+print("The sum is sum %s" % (sum))
---
+++ experitnpython/EIPC10whileloop.py
@@ -0,0 +1,14 @@
+# write a program to print numbers from 1 to 10 by using while loop
+x = 1
+while x <= 10:
+    print(x)
+    x = x + 1
+
+# write the sum fo fist n numbers:
+n = int(input("enter a number:"))
+sum = 0
+i = 1
+while i <= n:
+    sum = sum + i
+    i = i + 1
+print(sum)
