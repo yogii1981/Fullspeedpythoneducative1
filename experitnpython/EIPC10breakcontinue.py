d20d13f75673f9a7cc48dfb48b9c876a0e6dda0f
yogeshsharma <yogeshsharma1981@gmail.com>
2020-04-08 09:18:12 -0400

    commit
    Signed-off-by: yogeshsharma <yogeshsharma1981@gmail.com>

--- experitnpython/EIPC10breakcontinue.py
+++ experitnpython/EIPC10breakcontinue.py
@@ -14,3 +14,19 @@ for item in cart:
         print("To place this order insurance is required")
         break
     print(item)
+
+# continue to use "Continue"- this program will continue to print the numbers whenever the number divided by 2
+# and remainder is zero
+
+for i in range(10):
+    if i % 2 == 0:
+        continue
+    print(i)
+
+# Write a program which doesn't process items when price is more than certain number and print message for them
+cart = [10, 20, 30, 400, 50, 600, 700, 9000]
+for item in cart:
+    if item > 500:
+        print("Can't process this item:", item)
+        continue
+    print(item)
