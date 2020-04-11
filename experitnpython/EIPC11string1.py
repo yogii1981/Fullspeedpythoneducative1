4fa995ac69df955cc0c5e007d4342b3bb309cda0
yogeshsharma <yogeshsharma1981@gmail.com>
2020-04-11 10:07:16 -0400

    commit
    Signed-off-by: yogeshsharma <yogeshsharma1981@gmail.com>

--- experitnpython/EIPC11string1.py
+++ experitnpython/EIPC11string1.py
@@ -2,3 +2,14 @@
 s = "Learning Python is very difficult"
 s1 = s.replace("difficult", "easy")
 print(s1)
+
+# splitting of strings:
+s2 = "rocky mountain, this is a camp"
+l = s2.split(',')
+for x in l:
+    print(x)
+
+s3 = "10-12-1984"
+l = s3.split('-')
+for x in l:
+    print(x)
