a84b7c3ffb05f7e80381dedf25be50c07a5770fd
yogeshsharma <yogeshsharma1981@gmail.com>
2020-04-12 10:30:36 -0400

    commit
    Signed-off-by: yogeshsharma <yogeshsharma1981@gmail.com>

--- experitnpython/EIPC11string1.py
+++ experitnpython/EIPC11string1.py
@@ -26,3 +26,37 @@ print(s4.lower())
 print(s4.swapcase())
 print(s4.title())
 print(s4.capitalize())
+
+# format with strings and using replacement
+name = 'david'
+age = 48
+salary = 100000
+print("{} at {} will make {}".format(name, age, salary))
+print("{x} at {y} will make {z}".format(x=name, y=age, z=salary))
+
+#  write a program to display the input display in string
+# method1
+s = input("enter a string")
+print(s[::-1])
+
+# method2
+
+s = input("Enter some string:")
+i = len(s) - 1
+target = " "
+while i >= 0:
+    target = target + s[i]
+    i = i - 1
+print(target)
+
+# write a program to reverse order of words
+s = input("Enter a string:")
+l = s.split()
+print(s.split())
+l1 = []
+i = len(l) - 1
+while i >= 0:
+    l1.append(l[i])
+    i = i - 1
+output = ''.join(l1)
+print(output, end=' ')
