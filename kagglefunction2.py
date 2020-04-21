703b6c61926b63458bc6d2e1bd660dabeb6b78a4
yogeshsharma <yogeshsharma1981@gmail.com>
2020-04-20 20:49:25 -0400

    commit
    Signed-off-by: yogeshsharma <yogeshsharma1981@gmail.com>

--- kagglefunction2.py
+++ kagglefunction2.py
@@ -69,3 +69,22 @@ def modu1(x, y):
 
 
 print(modu1(16, 3))
+
+""" the candy-sharing friends Alice, Bob and Carol tried to split candies evenly. 
+For the sake of their friendship, any candies left over would be smashed. 
+For example, if they collectively bring home 91 candies, they'll take 30 each and smash 1.
+
+Below is a simple function that will calculate the number of candies to smash for 
+any number of total candies.
+
+Modify it so that it optionally takes a second argument representing the number 
+of friends the candies are being split between. If no second argument is provided, 
+it should assume 3 friends, as before."""
+
+
+def to_smash(total_candies, number_of_candidates):
+    """ return the number of candies to be smashed after equal distribution between 3 friends"""
+    return total_candies % number_of_candidates
+
+
+print(to_smash(127, 4))
