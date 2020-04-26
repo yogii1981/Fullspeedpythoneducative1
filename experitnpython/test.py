0780d3a604f4497f3c9e886b78c81eb5ad1a2d01
yogeshsharma <yogeshsharma1981@gmail.com>
2020-04-26 10:58:53 -0400

    commit
    Signed-off-by: yogeshsharma <yogeshsharma1981@gmail.com>

---
+++ experitnpython/test.py
@@ -0,0 +1,12 @@
+def f(a, b='b', c='c', type='T'):
+    print("Input", a, b, c, type)
+
+
+# print(f(1000))
+# print(f(a=1000))
+# print(f(a=100000,c='ccc'))
+# # print(f())
+# print(f(c='ccc',a=10000))
+# print(f('happy','life','rich'))
+
+print(f(actor='johny'))
