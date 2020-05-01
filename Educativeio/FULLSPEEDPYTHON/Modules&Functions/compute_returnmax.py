6f373081b7359a4132f73692d6f667f4a915b2f1
yogeshsharma <yogeshsharma1981@gmail.com>
2020-04-30 22:11:16 -0400

    commit
    Signed-off-by: yogeshsharma <yogeshsharma1981@gmail.com>

--- Educativeio/FULLSPEEDPYTHON/Modules&Functions/compute_returnmax.py
+++ Educativeio/FULLSPEEDPYTHON/Modules&Functions/compute_returnmax.py
@@ -26,27 +26,54 @@
 
 
 """Write a program to find maximum between n numbers
-Method-2  using the in-built max function"""
+Method-3  using the in-built max function"""
+
+# def findMax1(list1):
+#     # Assume first number in list is largest
+#     # initially and assign it to variable "max"
+#     max = list1[0]
+#
+#     # Now traverse through the list and compare
+#     # each number with "max" value. Whichever is
+#     # largest assign that value to "max'.
+#     for x in list1:
+#         if x > max:
+#             max = x
+#
+#             # after complete traversing the list
+#     # return the "max" value
+#     return max
+#
+#     # Driver code
+#
+#
+# list1 = [10, 20, 4, 45, 99]
+# print("Largest element is:", findMax1(list1))
 
 
-def findMax1(list1):
-    # Assume first number in list is largest
-    # initially and assign it to variable "max"
-    max = list1[0]
-
-    # Now traverse through the list and compare
-    # each number with "max" value. Whichever is
-    # largest assign that value to "max'.
-    for x in list1:
-        if x > max:
-            max = x
-
-            # after complete traversing the list
-    # return the "max" value
-    return max
-
-    # Driver code
+"""Write a program to find maximum between n numbers
+Method-3 user enters the numbers and condition"""
 
 
-list1 = [10, 20, 4, 45, 99]
-print("Largest element is:", findMax1(list1))
+def findMax2():
+    # number in a list
+
+    # creating empty list
+    list2 = []
+
+    # asking number of elements to put in the list
+    num = int(input("Enter number of elements in the list: "))
+
+    # iterating till num to append elements in list
+    # need to count the elements entered
+    count = 0
+    for i in range(1, num + 1):
+        elements = int(input("Enter elements: "))
+        list2.append(elements)  # appending list
+        count += 1
+        if count == num:
+            max3 = max(list2)
+            return max3
+
+
+print(findMax2())
