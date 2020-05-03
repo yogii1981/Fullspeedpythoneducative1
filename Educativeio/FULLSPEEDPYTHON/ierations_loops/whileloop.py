f3265740209deb229cca6205c7922b0ecb76a855
yogeshsharma <yogeshsharma1981@gmail.com>
2020-05-03 00:44:50 -0400

    commit
    Signed-off-by: yogeshsharma <yogeshsharma1981@gmail.com>

---
+++ Educativeio/FULLSPEEDPYTHON/ierations_loops/whileloop.py
@@ -0,0 +1,14 @@
+"""
+Finally, we also have the "while" statement that allows us to repeat a
+sequence of instructions while a specified condition is true.
+
+The general syntax for creating a while loop is:
+
+while(condition):
+    statement
+"""
+
+n = 10
+while n > 0:
+    print(n)
+    n = n - 1
