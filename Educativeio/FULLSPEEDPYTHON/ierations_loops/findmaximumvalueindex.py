1d2091001ccc23c2eee878837cf17558d06f0ef2
yogeshsharma <yogeshsharma1981@gmail.com>
2020-05-03 22:32:03 -0400

    commit
    Signed-off-by: yogeshsharma <yogeshsharma1981@gmail.com>

---
+++ Educativeio/FULLSPEEDPYTHON/ierations_loops/findmaximumvalueindex.py
@@ -0,0 +1,13 @@
+def findMaxvalueindex(list):
+    maximum = list[0]  # first value is assumed as maximum
+    index = 0
+    for i, value in enumerate(
+            list):  # loop which check whether any value in the loop is greater than first value or not
+        if value > maximum:
+            maximum = value
+            index = i
+    return ["Index is {} and maximum value is {}".format(index, maximum)]
+
+
+l = [1, 2, 3, 4]
+print(findMaxvalueindex(l))
