20ea86e8093dd3a39fbc03c226a0e4b3a36d156f
yogeshsharma <yogeshsharma1981@gmail.com>
2020-05-05 09:41:29 -0400

    commit
    Signed-off-by: yogeshsharma <yogeshsharma1981@gmail.com>

--- Educativeio/FULLSPEEDPYTHON/ierations_loops/PrintEvenOdd.py
+++ Educativeio/FULLSPEEDPYTHON/ierations_loops/PrintEvenOdd.py
@@ -25,3 +25,23 @@ def printEvenOdd(n):
 
 
 printEvenOdd(10)
+
+Languages = ["Python", "Java", "R"]
+for x in Languages:
+    if x == "Java":
+        break
+    print(x)
+
+    i = 1
+    while i < 7:
+        print(i)
+        if (i == 3):
+            break
+        i += 1
+
+i = 1
+while i <= 7:
+    i += 1
+    if (i == 3):
+        continue
+    print(i)
