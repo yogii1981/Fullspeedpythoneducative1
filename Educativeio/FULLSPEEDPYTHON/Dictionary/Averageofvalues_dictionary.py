9cd5f197fc03a0c5751a63a36ca80f0e456fc3ea
yogeshsharma <yogeshsharma1981@gmail.com>
2020-05-08 11:03:07 -0400

    commit
    Signed-off-by: yogeshsharma <yogeshsharma1981@gmail.com>

---
+++ Educativeio/FULLSPEEDPYTHON/Dictionary/Averageofvalues_dictionary.py
@@ -0,0 +1,17 @@
+def calculateAvg(ages):
+    length = len(ages)
+    return (sum(ages.values()) / length)
+
+
+ages = {
+    "Peter": 10,
+    "Isabel": 11,
+    "Anna": 9,
+    "Thomas": 10,
+    "Bob": 10,
+    "Joseph": 11,
+    "Maria": 12,
+    "Gabriel": 10,
+    "Raj": 0,
+}
+print(calculateAvg(ages))
---
+++ Educativeio/FULLSPEEDPYTHON/Dictionary/maxvalue_dictionary.py
@@ -0,0 +1,42 @@
+"""This program will maximum value for any student in this it is age of the oldest student"""
+
+
+def oldestStudentage(ages):
+    return (max(ages.values()))
+
+
+ages = {
+    "Peter": 10,
+    "Isabel": 11,
+    "Anna": 9,
+    "Thomas": 10,
+    "Bob": 10,
+    "Joseph": 11,
+    "Maria": 12,
+    "Gabriel": 10,
+    "Raj": 0,
+}
+print(oldestStudentage(ages))
+
+"""Display the name of the oldest student"""
+
+
+def oldestStudent(ages):
+    value = list(ages.values())
+    key = list(ages.keys())
+    return key[value.index(max(value))]
+
+
+ages = {
+    "Peter": 10,
+    "Isabel": 11,
+    "Anna": 9,
+    "Thomas": 10,
+    "Bob": 10,
+    "Joseph": 11,
+    "Maria": 12,
+    "Gabriel": 10,
+    "Raj": 0,
+}
+
+print(oldestStudent(ages))
