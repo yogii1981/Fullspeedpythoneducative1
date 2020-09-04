ba669574d57747ffb770577d5f5e1a98a42192d9
Yogesh Sharma <yogeshsharma1981@gmail.com>
2020-09-04 12:46:56 -0400

    commit
    Signed-off-by: Yogesh Sharma <yogeshsharma1981@gmail.com>

---
+++ Bite_size python/Proj2:Shopping for Science Supplies.py
@@ -0,0 +1,35 @@
+# Total Money given for experiment
+
+total_money = 25
+
+# Number of flowerpots
+No._flowerpot = input("Enter the number of flower pots")
+
+# Cost of Flowerpots
+
+cost_flowerpot = 4
+
+# Number of flowerseed bags
+o_of_flower_seedbags = input("Enter the number of flower seed bags")
+
+# cost of pack of flower seeds
+
+cost_flowerseed = 1
+
+# Number of soilbags
+No_soil_bag = input("Enter the number of soil bags")
+
+# cost of bag soil
+
+cost_bagofsoil = 5
+
+# tax
+
+tax = 0.06
+
+# She can purchase upt0
+
+cost_of_items = ((No._flowerpot * cost_flowerpot) + (No_of_flower_seedbags * cost_flowerseed) + (
+            No_soil_bag - cost_bagofsoil)) * tax
+_
+print(total_cost)
