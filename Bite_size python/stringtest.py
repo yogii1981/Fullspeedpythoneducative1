12fb88ee66ad0333d22e3b1451d4f167a9a09f08
Yogesh Sharma <yogeshsharma1981@gmail.com>
2021-03-16 09:23:07 -0400

    commit
    Signed-off-by: Yogesh Sharma <yogeshsharma1981@gmail.com>

---
+++ Bite_size python/stringtest.py
@@ -0,0 +1,57 @@
+testa = """
+This
+is 
+a test
+"""
+
+print(testa)
+
+# Escape Characters
+feedback = 'Teacher Said that "you shouldn\'t quit studies" '
+print(feedback)
+
+# Escape Characters- next line
+quote = 'Dream it.\n I know it.'
+print(quote)
+
+# Escape-capitalize
+name1 = 'bridget'
+print(name1.capitalize())
+
+# strip
+mood = '!!!happy!!!'
+print(mood.strip('!'))
+
+season = '    Summer'
+print(season.strip())
+
+# lower()
+
+whisper = "DO YOU WANT"
+print(whisper.lower())
+
+# upper
+yell = "today's is the day"
+print(yell.upper())
+
+# replace
+opinion = 'Learning is hard'
+print(opinion.replace('Learning', 'workout'))
+
+# len
+print(len(opinion))
+
+# concatenation
+first_name = 'Mile'
+last_name = 'Singhli'
+print
+
+for i in range(3):
+    print(i)
+
+python = ['cool']
+x = python in python
+
+print(x)
+
+print('\"hello' + " " + "world's end\"")
