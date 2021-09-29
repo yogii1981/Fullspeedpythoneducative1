d2615699b840d33380a3727791912b5dec1ec0ae
Yogesh Sharma <yogeshsharma1981@gmail.com>
2021-09-29 00:55:50 -0400

    commit
    Signed-off-by: Yogesh Sharma <yogeshsharma1981@gmail.com>

---
+++ Pythonprog_4in1_AndrewPark/Speed_along_lists_tuples_pythonallinone_chap3.py
@@ -0,0 +1,10 @@
+# lists
+scores = [99, 89, 12, 34, 56]
+print(type(scores))
+print(scores)
+
+# position or referencing of list item in lists
+print(scores[0])
+print(scores[-1])
+print(scores[0:3])
+print(scores[-1::-1])  # starting from the last to the first in the reverse order
