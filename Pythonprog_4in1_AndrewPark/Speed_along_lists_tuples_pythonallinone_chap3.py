adba4ecf6a1f18ffea050c836794445c98753427
Yogesh Sharma <yogeshsharma1981@gmail.com>
2021-09-29 22:27:38 -0400

    commit
    Signed-off-by: Yogesh Sharma <yogeshsharma1981@gmail.com>

--- Pythonprog_4in1_AndrewPark/Speed_along_lists_tuples_pythonallinone_chap3.py
+++ Pythonprog_4in1_AndrewPark/Speed_along_lists_tuples_pythonallinone_chap3.py
@@ -27,3 +27,33 @@ names = ["Raj", "Amit", "Ketan"]
 new_person = "Sahil"
 names.insert(1, new_person)
 print(names)
+names[0] = "Anjali"  # changing an item in a list
+print(names)
+
+# combining lists
+names = ["Raj", "Amit", "Ketan"]
+names1 = ["Anjali", "Rita"]
+names.extend(names1)
+print(names)
+
+# remove the item from list
+names2 = ["Raj", "Amit", "Ketan"]
+names2.remove("Amit")
+print(names2)
+
+letters = ["A", "B", "C", "B", "R", "C", "A"]
+letters.remove("C")  # only one "C" is removed
+print(letters)
+
+# in order to remove all "C" use while loop
+letters1 = ["A", "B", "C", "B", "R", "C", "A"]
+c_in_letters = letters1.count("C")  # Count the item "C" in the list
+print(c_in_letters)
+
+while c_in_letters > 0:  # While the list has count of item = "C"
+    if c_in_letters == 0:  # if count of item "C" == 0
+        print(letters1, "No C found")
+    else:
+        letters1.remove("C")  # remove the item "C" once
+        c_in_letters += 1
+        print(letters1)
