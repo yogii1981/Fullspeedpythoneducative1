cf7e07fbebd953afde06d41003df65ec4d727c97
Yogesh Sharma <yogeshsharma1981@gmail.com>
2021-09-29 23:39:22 -0400

    commit
    Signed-off-by: Yogesh Sharma <yogeshsharma1981@gmail.com>

--- Pythonprog_4in1_AndrewPark/Advance_loop.py
+++ Pythonprog_4in1_AndrewPark/Advance_loop.py
@@ -4,6 +4,6 @@ grades = ["A", "B", "C", "L", "E", "K", "A", "E", "D", "B"]
 # what you want to look for
 # look_for = "E"
 # e_index = grades.index(look_for)
-for n, i in enumerate(grades):
-    if i == "E":
-        print("The position of E is ", n)
+for position, value in enumerate(grades):
+    if value == "E":
+        print("The position of E is ", position)
