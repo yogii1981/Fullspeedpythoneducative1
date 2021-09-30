5fb1030386c7ce8d7c422bc4e8be5d751f14a7f6
Yogesh Sharma <yogeshsharma1981@gmail.com>
2021-09-29 20:31:58 -0400

    commit
    Signed-off-by: Yogesh Sharma <yogeshsharma1981@gmail.com>

--- Pythonprog_4in1_AndrewPark/Speed_along_lists_tuples_pythonallinone_chap3.py
+++ Pythonprog_4in1_AndrewPark/Speed_along_lists_tuples_pythonallinone_chap3.py
@@ -8,3 +8,22 @@ print(scores[0])
 print(scores[-1])
 print(scores[0:3])
 print(scores[-1::-1])  # starting from the last to the first in the reverse order
+# print(scores[7]) # this should give error as there is no list item at the given position (out of index)
+
+# Looping through a list
+scores = [99, 89, 12, 34, 56]
+for score in scores:
+    print("The {} is greater than 75".format(score))
+print("Done")
+
+# Add a list item to list
+names = ["Raj", "Amit", "Ketan"]
+names.append("Anuj")
+print(names)
+
+# insert a list item to certain position in the list
+
+names = ["Raj", "Amit", "Ketan"]
+new_person = "Sahil"
+names.insert(1, new_person)
+print(names)
