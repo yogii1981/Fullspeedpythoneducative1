9f36181a9cab5d3c1cb91e5ae18f572e3e9540e5
Yogesh Sharma <yogeshsharma1981@gmail.com>
2021-10-05 21:40:40 -0400

    commit
    Signed-off-by: Yogesh Sharma <yogeshsharma1981@gmail.com>

--- Thinkpython/Variable_expression_statements.py
+++ Thinkpython/Variable_expression_statements.py
@@ -35,7 +35,7 @@ print(volume)
 # # area(3,4)
 
 
-# Exercise 2.3
+# Exercise 2.2.2
 cover_price = 24.95
 discount = 40 * (1 / 100)
 afterdiscountpriceofbook = 24.95 * 0.6
@@ -43,13 +43,34 @@ shipping_cost_first = 3
 subsequent_shipping_cost = 0.75
 numofbookordered = 60
 
-for numofbookordered in range(61):  # for
-    if numofbookordered == 1:
+for numofbookordered in range(61):  # for book order number 60
+    if numofbookordered == 1:  # if only one book is ordered
         totalwholesalecost = (afterdiscountpriceofbook + shipping_cost_first)
         print(totalwholesalecost)
-        numofbookordered += 1
+        numofbookordered += 1  # if bookordered are more than one
     else:
         totalwholesalecost = (numofbookordered * afterdiscountpriceofbook) + (
-                (numofbookordered - 1) * 0.75) + shipping_cost_first
+                    (numofbookordered - 1) * 0.75) + shipping_cost_first
         print(totalwholesalecost)
 print(totalwholesalecost)
+
+# Exercise 2.2.3
+# start_time = (6 + 52/60.0)
+#
+# run_easypace  = 2 * (8 +15/ 60.0)/60
+# tempo_speed = 3 * (7 + 12/60.0)/60
+# totaltime = run_easypace + tempo_speed
+# print("Totaltime", totaltime)
+
+
+start_time = (6 * 60 + 52) * 60
+easy_time = (8 * 60 + 15) * 2
+tempo_time = (7 * 60 + 12) * 3
+
+breakfast_hour = (start_time + easy_time + tempo_time) / (60 * 60)
+breakfast_int_hour = int(breakfast_hour)
+
+breakfast_minute = (breakfast_hour - breakfast_int_hour) * 60
+breakfast_int_minute = int(breakfast_minute)
+
+print('Breakfast is at {}.{}'.format(breakfast_int_hour, breakfast_int_minute))
