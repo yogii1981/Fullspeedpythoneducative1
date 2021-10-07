320a58e1ad7f19a313a613ce971ab59e48125b9b
Yogesh Sharma <yogeshsharma1981@gmail.com>
2021-10-07 16:05:41 -0400

    commit
    Signed-off-by: Yogesh Sharma <yogeshsharma1981@gmail.com>

--- 1000Pythonexamples/calculations.py
+++ 1000Pythonexamples/calculations.py
@@ -1,3 +1,4 @@
+print(" Calculate the area and circumference of the rectangle")
 width = 23
 height = 17
 area = width * height
@@ -5,3 +6,52 @@ print("The area is: ", area)
 
 circumference = 2 * (width * height)
 print("The circumference is:", circumference)
+
+print()
+print("write a script to calculate the area and circumference of the circle")
+
+
+def radius(a):
+    r = a
+    return r
+
+
+print(radius(7))
+
+import math
+
+areacircle = math.pi * radius(7) ** 2
+print("The area of circle:", areacircle)
+
+circumferencecircle = 2 * math.pi * radius(7)
+print("The circumeference of circle:", circumferencecircle)
+
+print()
+print(" Write a script that has two numbers a and b and calculate a + b, a - b, a * b , a / b ")
+
+
+def sumofinteger(a, b):
+    sum = a + b
+    return sum
+
+
+def diffferenceofinteger(a, b):
+    difference = a - b
+    return difference
+
+
+def multiplicationofinteger(a, b):
+    multi = a * b
+    return multi
+
+
+def divisionofinteger(a, b):
+    divis = a / b
+    return divis
+
+
+print()
+print(sumofinteger(3, 4))
+print(diffferenceofinteger(3, 4))
+print(multiplicationofinteger(3, 4))
+print(divisionofinteger(3, 4))
