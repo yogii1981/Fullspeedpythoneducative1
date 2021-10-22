e31e52c6b730ffa854dad5e8f8810076f3b49157
Yogesh Sharma <yogeshsharma1981@gmail.com>
2021-10-21 23:13:03 -0400

    commit
    Signed-off-by: Yogesh Sharma <yogeshsharma1981@gmail.com>

--- 1000Pythonexamples/calculations.py
+++ 1000Pythonexamples/calculations.py
@@ -287,3 +287,33 @@ print(a, "is a random generated number")
 fruits = ['apple', 'banana', 'peach', 'orange', 'durian', 'papaya']
 salad = random.sample(fruits, 3)
 print(salad)
+
+# short circuit fixed
+#
+#
+# def check_money():
+#     return  money > 1000000
+#
+# def check_salary():
+#     salary += 1
+#     return salary >= 1000
+#
+# while True:
+#     if check_money() or check_salary():
+#         print("I can live")
+
+# Triple quaoted Springs
+
+text = """ first row
+second row
+third row"""
+
+print(text)
+
+text1 = "Hello World"
+text2 = """Hello 
+World"""
+
+print("Length of text1:", len(text1))
+
+print("Length of text2:", len(text2))
