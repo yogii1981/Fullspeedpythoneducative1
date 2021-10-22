f40b3175bbcc17d4c178794793c19736234093b5
Yogesh Sharma <yogeshsharma1981@gmail.com>
2021-10-22 05:39:22 -0400

    commit
    Signed-off-by: Yogesh Sharma <yogeshsharma1981@gmail.com>

--- 1000Pythonexamples/Loop.py
+++ 1000Pythonexamples/Loop.py
@@ -3,21 +3,34 @@
 # While - repeat an action till conditiion is met or stopped being met
 
 # For loop in strings
+print("program1")
 txt = "Hello world!"
 for char in txt:
     print(char)
 
+print("program2")
 numb1 = range(1, 200)
 for i in numb1:
     print(i in numb1)
 
+print("program3")
 for i in range(3, 7):
     print(i)
 
 # For - In loop - early break
-
+print("program4")
 txt = 'Hello World'
 for c in txt:
     if c == " ":
         break
     print(c)
+
+# For - In loop - parts using continue
+print("program5")
+txt = 'Hello World'
+for c in txt:
+    if c == " ":
+        continue
+    print(c)
+
+print("Program1 print a space between 'hello and 'world' but program 5 doesn't ")
